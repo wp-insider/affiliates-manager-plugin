6d4167dc5bac721b696b89593e343d3ce11ee984
affmngr <affmngr@gmail.com>
2015-08-11 15:58:52 +1000

    .updated the POT file in the plugin

---
+++ wpaffiliatemanager/source/dynamic-translatable-strings.php
@@ -0,0 +1,8 @@
+<?php
+/* These strings are generated dynamically. We have mentioned them here so the plugin can at least translate the strings. */
+//affiliate_register_form.php
+_e( 'Address Line 1', 'wpam' );
+_e( 'Address Line 2', 'wpam' );
+_e( 'Zip Code', 'wpam' );
+_e( 'Company Name', 'wpam' );
+_e( 'Website URL', 'wpam' );
